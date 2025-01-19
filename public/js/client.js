271e84b813d3f8ce83cc8626a1df5cf7a98859ca
kyle <nojaim.k@gmail.com>
2025-01-19 18:31:08 -0500

    definitely in getAvailDist, still working on all conditions

--- public/js/client.js
+++ public/js/client.js
@@ -24,30 +24,32 @@ class Game{
     update(){
         let newX = this.player.x;
         let newY = this.player.y;
-
         const isDiagonal = (this.input.keys.up || this.input.keys.down) && (this.input.keys.left || this.input.keys.right);
         const speed = isDiagonal ? this.player.speed / Math.sqrt(2) : this.player.speed;
-
+        
         if (isDiagonal){
             if (this.input.keys.up) newY -= speed;
             if (this.input.keys.down) newY += speed;
             if (this.input.keys.right) newX += speed;
             if (this.input.keys.left) newX -= speed;
-
+            
+            console.log({newX, newY})
             const diagonalDistance = this.map.getAvailableDistance(this.player.x, this.player.y, newX, newY, this.player.width, this.player.height);
 
-            if (diagonalDistance.x === 0 || diagonalDistance.y === 0){
+            if (diagonalDistance.x === 0) {
+                // Only X is blocked, reset just X and recalculate
                 newX = this.player.x;
+                if (this.input.keys.left) newX -= this.player.speed;
+                if (this.input.keys.right) newX += this.player.speed;
+                // Keep Y at diagonal speed since it's not blocked
+                this.player.y += diagonalDistance.y;
+            } else if (diagonalDistance.y === 0) {
+                // Only Y is blocked, reset just Y and recalculate
                 newY = this.player.y;
-
                 if (this.input.keys.up) newY -= this.player.speed;
                 if (this.input.keys.down) newY += this.player.speed;
-                if (this.input.keys.right) newX += this.player.speed;
-                if (this.input.keys.left) newX -= this.player.speed;
-
-                const distance = this.map.getAvailableDistance(this.player.x, this.player.y, newX, newY, this.player.width, this.player.height);
-                this.player.x += distance.x;
-                this.player.y += distance.y;
+                // Keep X at diagonal speed since it's not blocked
+                this.player.x += diagonalDistance.x;
             }else{
                 this.player.x += diagonalDistance.x;
                 this.player.y += diagonalDistance.y;
