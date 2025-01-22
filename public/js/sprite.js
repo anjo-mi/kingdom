982970aa01509db1cbdde70cbaeba8b87efce519
kyle <nojaim.k@gmail.com>
2025-01-21 21:26:31 -0500

    need to move sprite into games player

--- public/js/sprite.js
+++ public/js/sprite.js
@@ -78,4 +78,43 @@ export class Sprite{
     addAnimation(animation){
         this.animations.push(animation);
     }
+
+    startAnimation(index){
+        if (index >= 0 && index < this.animations.length){
+            this.currentAnimation = this.animations[index];
+            this.currentAnimation.isRunning = true;
+            this.currentAnimation.currentFrame = 0;
+        }
+    }
+
+    stopAnimation(){
+        if (this.currentAnimation){
+            this.currentAnimation.isRunning = false;
+            // ?????????? will this leave it frozen on last walking animation???????
+            // this.currentAnimation.currentFrame = 0;
+        }
+    }
+
+    update(){
+        if (!this.currentAnimation || !this.currentAnimation.isRunning || this.currentAnimation.isPaused) return;
+
+        this.currentAnimation.frameCount = (this.currentAnimation.frameCount || 0) + 1
+
+        if (this.currentAnimation.frameCount >= this.currentAnimation.frameRate){
+            this.currentAnimation.frameCount = 0;
+            this.currentAnimation.currentFrame++;
+
+            if(this.currentAnimation.currentFrame >= this.currentAnimation.frames.length){
+                this.currentAnimation.currentFrame = 0;
+                if (!this.currentAnimation.loop){
+                    this.stopAnimation()
+                }
+            }
+        }
+    }
+
+    getCurrentFrame(){
+        if (!this.currentAnimation) return null;
+        return this.currentAnimation.frames[this.currentAnimation.currentFrame];
+    }
 }
